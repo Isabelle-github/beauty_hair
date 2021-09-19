3f949cbb0e8eeb36a7dd3908e73a8217019eb494
Avia <avia@Avias-Air.fritz.box>
2021-09-19 12:40:09 +0200

    iframe

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,18 @@ const Contact = () => {
       <div className="parallax bgContact">
         <h1>contact</h1>
       </div>
+      <div class="map">
+        <iframe
+          width="1046"
+          height="399"
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?q=15%20Wordsworth%20Ave,%20Doncaster%20DN4%208JU,%20%D7%91%D7%A8%D7%99%D7%98%D7%A0%D7%99%D7%94&t=k&z=17&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+        ></iframe>
+      </div>
     </main>
   );
 };
